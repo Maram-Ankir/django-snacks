15e2fbe31a104f8ca80d6bfec2fb619e8cfce237
Maram-Ankir <maramankir5@gmail.com>
2021-08-29 20:38:35 +0300

    feat:edit tests

--- snacks/tests.py
+++ snacks/tests.py
@@ -7,17 +7,14 @@ class thingsTests(SimpleTestCase):
     def test_home_page_status_code(self):
         url = reverse('home')
         response = self.client.get(url)
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, 200)
 
     def test_about_page_status_code(self):
         url = reverse('about')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
-    def test_contact_page_status_code(self):
-        # url = reverse('contact')
-        response = self.client.get('/contact')
-        self.assertEqual(response.status_code, 200)
+
     
     def test_home_page_templete(self):
         url = reverse('home')
